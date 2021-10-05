f7f66055885523cccbb9d423e79717abc8353f8d
Kyle Eggelhoefer <keggelhoefer@gmail.com>
2021-10-05 16:31:04 +0100

    content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,72 @@
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <section>
+      <div className="uk-background-fixed uk-background-center-center uk-height-medium uk-width-full" id="backgroundimage">
+        <section>
+          <h1>Kyle-Egg</h1>
+          <h3>Hi, I am Kyle Eggelhoefer</h3>
+          <h3>Full-Stack Developer</h3>
+        </section>
+        <section>
+          <h2>About</h2>
+          <p>I am a newly graduated Software Engineer having worked in previous roles in marketing in the hospitality industry.</p>
+          <p>I made the switch in my career due to my interest in technology, my determination to learn and problem solve in the ever changing sector.</p>
+          <p>With my previous experience in Marketing, I understand the importance of producing aesthetically pleasing, high quality work. In addition, the design skills I have developed during that time aids my ability to build and produce programmes with creative flair and a high quality user experience. </p>
+        </section>
+        <section>
+          <h2>Skills</h2>
+          <p>Front-end: HTML, CSS/SASS, JavaScript, React, Axios, JSON, Bulma, Bootstrap, Python, Django, Node, MongoDB, Express,
+        PostgreSQL, Pipenv, Github, Herokuapp, Google Analytics, Photohshop, Illustrator, After Effects, Premier Pro</p>
+        </section>
+        <section>
+          <h2>Experience</h2>
+          <h3>Software Engineering Immersive Student</h3>
+          <h3>General Assembly</h3>
+          <h5>June - September 2021</h5>
+          <p>Enrolled into a 3 month Software Engineering bootcamp, learning front-end, back-end and full-stack development and frameworks, creating a broad range of individual and team projects.</p>
+        </section>
+        <section>
+          <h2>Projects</h2>
+          <div>
+            <h3>Craft Gin Shop</h3>
+            <p>or the final project, I opted to work alone and created an e-commerce Craft Gin website, when tasked with the brief to create a full-stack Django and React application with CRUD functionality within one week.</p>
+          </div>
+          <div>
+            <h3>Binge</h3>
+            <p>In a team of three, we collaborated to create a MERN application with CRUD functionality, using an Express API from a Mongo Database. We split the workload amongst ourselves, where I handled the majority of the front-end of the application. We created a Netflix-styled application to aid your choice of movie based on time, with ‘marathon’ and ‘sprint’ options. 
+            </p>
+          </div>
+          <div>
+            <h3>Pub Quiz Generator</h3>
+            <p>In a team of two and with only two days to create the project, my partner and I worked together via VS Code Live Share, where we were tasked to create a React application that consumed a public API and chose to create a Pub Quiz Generator.
+            </p>
+          </div>
+          <div>
+            <h3>Hoover Invaders</h3>
+            <p>My first project was to build a Space Invaders game using HTML, CSS and JavaScript in one week.</p>
+          </div>
+        </section>
+        <section>
+          <h2>Interests</h2>
+          <div>
+            <h3>Snowboarding</h3>
+            <p>Try to go once a year, previous destinations include: Val d`Isère, St. Anton, Whistler and Verbier.
+            </p>
+          </div>
+          <div>
+            <h3>Football</h3>
+            <p>Played in every previous employer`s football team, taking part in yearly tournaments when organised. </p>
+          </div>
+        </section>
+        <section>
+          <h2>Contact</h2>
+          <h4>Github</h4>
+          <h4>Linkedin</h4>
+          <h4>E-mail</h4>
+        </section>
+      </div>
+    </section>
+  )
 }
 
 export default App
