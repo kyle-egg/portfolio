dbe083f9265f581758f2199784e93f3a1f21d857
Kyle Eggelhoefer <keggelhoefer@gmail.com>
2021-10-13 17:06:16 +0100

    brighten logos

--- src/App.js
+++ src/App.js
@@ -120,7 +120,7 @@ function App() {
             </div>
             <div className="uk-animation-toggle" tabIndex="0">
               <div className="uk-card uk-card-default uk-card-body uk-animation-fade">
-                <svg width="50" height="50" viewBox="0 0 128 128">
+                <svg id="brighten" width="50" height="50" viewBox="0 0 128 128">
                   <path d="M90.9 61.6l.1-2V59h-.8c-1.2 0-2.5.3-3.4.9-1.4.8-2.2 2.3-2.2 4.4 0 3 1.5 4.7 4.1 4.7.8 0 1.3-.1 2.3-.4v-4.3l-.1-2.7zm-74.6 2.6c0 3.2 1.6 4.9 4.7 4.9.7 0 1 0 2-.2v-9.5c-1-.2-1.2-.3-2-.3-3 0-4.7 1.9-4.7 5.1zm29.6 3.9c0 1.2.7 1.7 2.3 1.7.9 0 1.8-.1 2.8-.3v-4c-4 .4-5.1 1-5.1 2.6zm63-9.1c-2.3 0-3.7 1.9-3.7 5.2 0 3.3 1.3 5.2 3.7 5.2 2.3 0 3.6-1.9 3.6-5.2 0-3.2-1.3-5.2-3.6-5.2zM126 43.9c0-3.8-3.1-6.9-6.9-6.9H7.9C4.1 37 1 40.1 1 43.9V84c0 3.9 3.1 7 6.9 7H119c3.8 0 6.9-3.1 6.9-6.9V43.9zM10.3 64.3c0-6 4-10 10.2-10 1 0 1.6.1 2.6.3V46h6v27c-3 .6-5.3.8-7.7.8-7.3 0-11.1-3.2-11.1-9.5zM38 68.5c0 4.7-.2 7-1.2 9-1 1.9-2.1 3.1-4.7 4.4l-5.5-2.6c2.6-1.2 3.7-2.3 4.5-4 .8-1.7.9-3.7.9-8.8V55h6v13.5zM38 52h-6v-6h6v6zm18 21.3c-2 .4-5.8.7-8.3.7-5 0-7.1-1.7-7.1-5.6 0-4.2 3.3-6.1 10.3-6.7v-1.3c0-1.1-.5-1.5-2-1.5-2.2 0-5 .6-7 1.8v-4.6c3-1.2 5.1-1.8 7.8-1.8 3 0 4.8.8 5.7 2.4.5.9.5 2 .5 4.5v12.1zm21 .7h-6V62.2c0-2.3-1-3.1-3.1-3.1-.8 0-1.9.1-2.9.4V74h-6V55.7c4-.9 6.5-1.3 9.4-1.3 3 0 5.4.7 6.7 2C76.4 57.7 77 59 77 62v12zm19.3-11.6L97 68v1.7c0 5.1-.6 7.5-1.9 9.4-1.8 2.9-5.1 4.3-9.7 4.3-2.3 0-4.5-.3-6.5-1.2v-5.5c2 1.1 4.2 1.6 6.4 1.6 3.9 0 5.6-1.6 5.6-5.3v-.1c-1 .6-2.4.8-3.9.8-5.2 0-8.5-3.4-8.5-8.8 0-6.7 4.9-10.5 13.5-10.5 2.5 0 4.9.3 7.7.8l-2.3 4.3c-1.6-.3 1.3 0-.7-.2m12.1 14.8c-5.9 0-9.6-3.7-9.6-9.7 0-6.2 3.8-10.1 9.8-10.1 5.9 0 9.5 3.7 9.5 9.8.1 6.1-3.7 10-9.7 10z" fill="#003A2B"></path>
                 </svg> 
                 <p className="uk-text-center">Django</p>
@@ -212,7 +212,7 @@ function App() {
             </div>
             <div className="uk-animation-toggle" tabIndex="0">
               <div className="uk-card uk-card-default uk-card-body uk-animation-fade">
-                <svg width="50" height="50" viewBox="0 0 128 128">
+                <svg id="brighten" width="50" height="50" viewBox="0 0 128 128">
                   <path fill="#1F0740" d="M87 52.4c-7.5.9-7.5 9.2-7.5 9.2h14.9c.1 0 .8-9.2-7.4-9.2zM38.2 63.1H51l-6.4-24.4z"></path><path fill="#1F0740" d="M0 0v128h128V0H0zm57.5 88.6L53 72.5H36.2l-4.4 16.1h-9.4l16-54.9v-3.8h12.2l17.3 58.7H57.5zm46-19.6h-24c1.9 19.2 21.2 10 21.2 10v8s-1.3 2.6-14.8 2.6-16.3-18.3-16.3-18.3v-4.7s1.3-22 17.3-22 16.5 14.6 16.5 14.6V69z"></path>
                 </svg> 
                 <p className="uk-text-center">After Effects</p>
@@ -220,7 +220,7 @@ function App() {
             </div>
             <div className="uk-animation-toggle" tabIndex="0">
               <div className="uk-card uk-card-default uk-card-body uk-animation-fade">
-                <svg width="50" height="50" viewBox="0 0 128 128">
+                <svg id="brighten" width="50" height="50" viewBox="0 0 128 128">
                   <path fill="#2A0634" d="M50.3 38.5h-7.4v20.7h7.4c5 0 9.1-4.1 9.1-9.1v-2.4c0-5.1-4.1-9.2-9.1-9.2z"></path><path fill="#2A0634" d="M0 0v128h128V0H0zm51.2 67.5h-8.3v21.3h-9.6V30.3h18.5c9.4-.1 17.1 7.4 17.2 16.8v2.3c0 9.9-8 18-17.8 18.1zm46.1-14.2s-7 0-10.1 1.3v34.2H77.1V48.9s10.2-5.1 20.2-3.8v8.2z"></path>
                 </svg> 
                 <p className="uk-text-center">Premier Pro</p>
@@ -291,7 +291,9 @@ function App() {
                     <a href="https://github.com/kyle-egg/project-three-frontend" target="_blank" rel="noreferrer">
                       <img className="contacticons" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" />
                     </a>
-                    <img className="contacticons" src="https://img.icons8.com/external-prettycons-solid-prettycons/50/000000/external-link-social-media-prettycons-solid-prettycons.png"/>
+                    <a href="https://binge-sei.netlify.app/" target="_blank" rel="noreferrer">
+                      <img className="contacticons" src="https://img.icons8.com/external-prettycons-solid-prettycons/50/000000/external-link-social-media-prettycons-solid-prettycons.png"/>
+                    </a>
                   </div>
                 </div>
               </div>
